9db6e26886286a09ad6ed4dbe64b0e380e9324c7
Simeon Petrov <simonpetrov85@gmail.com>
2023-03-05 20:54:55 +0200

    Folder renaming

--- JS Object and Classes/Lab/01.person.js
+++ JS Object and Classes/Lab/01.person.js
@@ -7,3 +7,8 @@ function person(firstName, lastName, age) {
     return person;
 }
 person("Peter", "Pan", "20");
+
+/**1.	Person Info
+Write a function that receives 3 parameters, sets them to an object, and returns that object.
+The input comes as 3 separate strings in the following order: firstName, lastName, age.
+ */
--- JS Object and Classes/Lab/02.city.js
+++ JS Object and Classes/Lab/02.city.js
@@ -10,3 +10,9 @@ city({
     country: "Bulgaria",
     postCode: "1000"
 });
+
+/**2.	City
+Write a function that receives a single parameter – an object, containing five properties:
+{ name, area, population, country, postcode }
+Loop through all the keys and print them with their values in format: "{key} -> {value}"
+ */
