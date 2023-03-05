bf8fcc1a72edd0cbe9c61232f20ad39036e83e71
Simeon Petrov <simonpetrov85@gmail.com>
2023-03-05 20:50:44 +0200

    Practice

---
+++ JS Object and Classes/Lab/01.person.js
@@ -0,0 +1,9 @@
+function person(firstName, lastName, age) {
+    let person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+    }
+    return person;
+}
+person("Peter", "Pan", "20");
---
+++ Аrrays-Advanced - Exercises/10.buildAWall.js
@@ -0,0 +1,14 @@
+function buildAWall(data) {
+    let numbers = data.map(number => number = Number(number));
+    
+
+
+
+
+
+
+    console.log(numbers[0]);
+}
+buildAWall([21, 25, 28]);
+//buildAWall([17]);
+//buildAWall([17, 22, 17, 19, 17]);
