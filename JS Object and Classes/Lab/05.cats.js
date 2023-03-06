39e3a726e1b451a2c50fc06a75ebf8e63316a026
Simeon Petrov <simonpetrov85@gmail.com>
2023-03-06 13:23:21 +0200

    Practice.

--- JS Object and Classes/Lab/05.cats.js
+++ JS Object and Classes/Lab/05.cats.js
@@ -21,3 +21,10 @@ function cats(array) {
     }
 }
 cats(['Mellow 2', 'Tom 5']);
+
+/**5.	Cats
+Write a function that receives array of strings in the following format '{cat name} {age}'.
+Create a Cat class that receives in the constructor the name and the age parsed from the input. 
+It should also have a method named "meow" that will print "{cat name}, age {age} says Meow" on the console.
+For each of the strings provided, you must create a cat object and invoke the .meow () method.
+ */
--- JS Object and Classes/Lab/06.songs.js
+++ JS Object and Classes/Lab/06.songs.js
@@ -23,8 +23,13 @@ function songs(array) {
         } else if (typeList === 'all') console.log(song.name);
     }
 }
-//songs([3, 'favourite_DownTown_3:14', 'favourite_Kiss_4:16', 'favourite_Smooth Criminal_4:01','favourite']);
-
-//songs([4, 'favourite_DownTown_3:14', 'listenLater_Andalouse_3:24', 'favourite_In To The Night_3:58', 'favourite_Live It Up_3:48', 'listenLater']);
-
+songs([3, 'favourite_DownTown_3:14', 'favourite_Kiss_4:16', 'favourite_Smooth Criminal_4:01','favourite']);
+songs([4, 'favourite_DownTown_3:14', 'listenLater_Andalouse_3:24', 'favourite_In To The Night_3:58', 'favourite_Live It Up_3:48', 'listenLater']);
 songs([2, 'like_Replay_3:15', 'ban_Photoshop_3:48', 'all']);
+
+/**6.	Songs
+Define a class Song, which holds the following information about songs: typeList, name, and time.
+You will receive the input as an array.
+The first element n will be the number of songs. Next n elements will be the songs data in the following format: "{typeList}_{name}_{time}", and the last element will be typeList / "all".
+Print only the names of the songs, which have the same typeList (obtained as the last parameter). If the value of the last element is "all", print the names of all the songs.
+ */
