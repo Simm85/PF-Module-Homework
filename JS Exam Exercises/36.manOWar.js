1813e2e776578a0d23b196ce16df51ed3e62506d
Simeon Petrov <simonpetrov85@gmail.com>
2023-04-04 20:55:35 +0300

    Homework practice.

---
+++ JS Exam Exercises/36.manOWar.js
@@ -0,0 +1,105 @@
+function manOWar(input) {
+    let pirateShip = input.shift().split('>').map(n => Number(n));
+    let warShip = input.shift().split('>').map(n => Number(n));
+    const maxHealth = Number(input.shift());
+    for (const line of input) {
+        let [command, ...values] = line.split(' ');
+        values = values.map(n => Number(n));
+        switch (command) {
+            case 'Fire': {
+                let [index, damage] = values;
+                if (index >= 0 && index < warShip.length) warShip[index] -= damage;
+                if (warShip[index] <= 0)
+                    return console.log("You won! The enemy ship has sunken.");
+            } break;
+
+            case 'Defend':
+                let [startIndex, endIndex, damage] = values;
+                const isValid = (startIndex >= 0 && startIndex < pirateShip.length) && (endIndex >= 0 && endIndex < pirateShip.length);
+                if (isValid) {
+                    while (startIndex <= endIndex) {
+                        pirateShip[startIndex] -= damage;
+                        if (pirateShip[startIndex] <= 0) return console.log("You lost! The pirate ship has sunken.");
+                        startIndex++;
+                    }
+                }
+                break;
+
+            case 'Repair':
+                let [index, health] = values;
+                if (index >= 0 && index < pirateShip.length) {
+                    pirateShip[index] += health;
+                    if (pirateShip[index] > maxHealth) pirateShip[index] = maxHealth;
+                }
+                break;
+
+            case 'Status':
+                const percent = 0.2 * maxHealth;
+                let forRepair = 0;
+                for (let i = 0; i < pirateShip.length; i++) {
+                    if (pirateShip[i] < percent) forRepair++;
+                }
+                console.log(`${forRepair} sections need repair.`);
+                break;
+
+            case 'Retire':
+                let sum = 0
+                pirateShip.forEach(section => sum += section);
+                console.log(`Pirate ship status: ${sum}`);
+                sum = 0;
+                warShip.forEach(section => sum += section);
+                console.log(`Warship status: ${sum}`);
+                break;
+        }
+    }
+}
+manOWar(["12>13>11>20>66",
+    "12>22>33>44>55>32>18",
+    "70",
+    "Fire 2 11",
+    "Fire 8 100",
+    "Defend 3 6 11",
+    "Defend 0 3 5",
+    "Repair 1 33",
+    "Status",
+    "Retire"
+]);
+
+console.log('=======================================================================================');
+
+manOWar(["2>3>4>5>2",
+    "6>7>8>9>10>11",
+    "20",
+    "Status",
+    "Fire 2 3",
+    "Defend 0 4 11",
+    "Repair 3 18",
+    "Retire"
+]);
+
+/**Problem 3 - Man-O-War
+Problem for exam preparation for the Programming Fundamentals Course @SoftUni.
+Submit your solutions in the SoftUni judge system at https://judge.softuni.org/Contests/Practice/Index/1773#2.
+
+The pirates encounter a huge Man-O-War at sea. 
+Create a program that tracks the battle and either chooses a winner or prints a stalemate. On the first line, you will receive the status of the pirate ship, which is a string representing integer sections separated by ">". On the second line, you will receive the same type of status, but for the warship: 
+"{section1}>{section2}>{section3}… {sectionn}"
+On the third line, you will receive the maximum health capacity a section of the ship can reach. 
+The following lines represent commands until "Retire":
+•	"Fire {index} {damage}" - the pirate ship attacks the warship with the given damage at that section. Check if the index is valid and if not, skip the command. If the section breaks (health <= 0) the warship sinks, print the following and stop the program: "You won! The enemy ship has sunken."
+•	"Defend {startIndex} {endIndex} {damage}" - the warship attacks the pirate ship with the given damage at that range (indexes are inclusive). Check if both indexes are valid and if not, skip the command. If the section breaks (health <= 0) the pirate ship sinks, print the following and stop the program:
+"You lost! The pirate ship has sunken."
+•	"Repair {index} {health}" - the crew repairs a section of the pirate ship with the given health. Check if the index is valid and if not, skip the command. The health of the section cannot exceed the maximum health capacity.
+•	"Status" - prints the count of all sections of the pirate ship that need repair soon, which are all sections that are lower than 20% of the maximum health capacity. Print the following:
+"{count} sections need repair."
+In the end, if a stalemate occurs, print the status of both ships, which is the sum of their individual sections, in the following format:
+"Pirate ship status: {pirateShipSum}
+Warship status: {warshipSum}"
+Input
+•	On the 1st line, you are going to receive the status of the pirate ship (integers separated by '>')
+•	On the 2nd line, you are going to receive the status of the warship
+•	On the 3rd line, you will receive the maximum health a section of a ship can reach.
+•	On the following lines, until "Retire", you will be receiving commands.
+Output
+•	Print the output in the format described above.
+ */
